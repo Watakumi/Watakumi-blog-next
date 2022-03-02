9c79786aa416b0a9a12858a52410b73c774590e7
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-03 01:51:46 +0900

    使用しないcssの削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout';
 import Top from '../components/Top';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>WatakumiBlog</title>
         <meta name="description" content="This is Watakumi Blog." />
