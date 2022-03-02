931627442c527a3984e112d6486ed47eafbb562f
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-03 02:13:52 +0900

    余白の調整

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC = ({ children }) => {
           <div className="flex h-24 flex-wrap items-center justify-between border-b border-gray-200 px-2 font-medium">
             <div className="flex h-full w-1/4 items-center pr-4">image</div>
             <div className="flex w-1/4 justify-end">
-              <div className="flex flex-row items-center justify-center text-center">
+              <div className="flex flex-row items-center justify-center text-center gap-2">
                 <a
                   href="#"
                   className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
