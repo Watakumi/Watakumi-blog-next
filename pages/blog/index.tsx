e7f5e59290a8621686457a386403fab82fbca36a
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:42:00 +0900

    /blog の追加

---
+++ pages/blog/index.tsx
@@ -0,0 +1,14 @@
+import { Card } from '../../components/Card';
+
+export default function Blog(): JSX.Element {
+  return (
+    <div>
+      <h1>All Posts</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
