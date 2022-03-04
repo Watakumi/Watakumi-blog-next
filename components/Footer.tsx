e26b8d58b2e2e56150ddcee3d8630f90e18ef75d
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:25:58 +0900

    Extract Footer Component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer(): JSX.Element {
+  return (
+    <footer className="mt-4">
+      <div>&copy; 2022 WatakumiBlog</div>
+    </footer>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from './Footer';
 import { Header } from './Header';
 
 const Layout: React.FC = ({ children }) => {
@@ -5,9 +6,7 @@ const Layout: React.FC = ({ children }) => {
     <div className="flex flex-col px-2 sm:px-8 md:px-24 lg:px-48 xl:px-60">
       <Header />
       <main>{children}</main>
-      <footer className="mt-4">
-        <div>&copy; 2022 WatakumiBlog</div>
-      </footer>
+      <Footer />
     </div>
   );
 };
