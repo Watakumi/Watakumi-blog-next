c5af6992fad240fe99c567fbd4f9f56b1393126b
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:22:37 +0900

    TopComponentでCardを使用する

--- components/Top.tsx
+++ components/Top.tsx
@@ -1,7 +1,13 @@
+import { Card } from './Card';
+
 export default function Top() {
   return (
     <div>
-      <h1>WatakumiBlog</h1>
+      <h1 className="text-4xl">Latest</h1>
+      <p className="mb-4 text-gray-800">
+        RubyやJavaScriptに関する記事を公開しています。
+      </p>
+      <Card />
     </div>
   );
 }
