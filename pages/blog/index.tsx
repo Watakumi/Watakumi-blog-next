4a70482271ccc22f2588faf8e2b2a5c65971d9f6
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:46:21 +0900

    styleの調整

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,7 +3,7 @@ import { Card } from '../../components/Card';
 export default function Blog(): JSX.Element {
   return (
     <div>
-      <h1>All Posts</h1>
+      <h1 className="text-4xl">All Posts</h1>
       <Card />
       <Card />
       <Card />
