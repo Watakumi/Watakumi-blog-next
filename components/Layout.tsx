a9a66a464bf6605e776e43573da3305e4bcd70fa
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:24:07 +0900

    Extract HeaderComponent

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,35 +1,9 @@
+import { Header } from './Header';
+
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col px-2 sm:px-8 md:px-24 lg:px-48 xl:px-60">
-      <header className="px-6 pb-12 antialiased">
-        <nav className="z-50 h-24 select-none">
-          <div className="flex h-24 flex-wrap items-center justify-between border-b border-gray-200 px-2 font-medium">
-            <div className="flex h-full w-1/4 items-center pr-4">image</div>
-            <div className="flex w-1/4 justify-end">
-              <div className="flex flex-row items-center justify-center text-center gap-2">
-                <a
-                  href="#"
-                  className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-                >
-                  Blog
-                </a>
-                <a
-                  href="#"
-                  className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-                >
-                  Tags
-                </a>
-                <a
-                  href="#"
-                  className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-                >
-                  Projects
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
+      <Header />
       <main>{children}</main>
       <footer className="mt-4">
         <div>&copy; 2022 WatakumiBlog</div>
