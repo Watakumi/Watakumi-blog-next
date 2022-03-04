ecf13d5c764e1c8772e9ff05b5713d2e77e16084
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:46:14 +0900

    遷移できるようにする

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,22 @@
+import Link from 'next/link';
+
 export function Header(): JSX.Element {
   return (
     <header className="px-6 pb-12 antialiased">
       <nav className="z-50 h-24 select-none">
         <div className="flex h-24 flex-wrap items-center justify-between border-b border-gray-200 px-2 font-medium">
-          <div className="flex h-full w-1/4 items-center pr-4">image</div>
+          <div className="flex h-full w-1/4 items-center pr-4">
+            <Link href={'/'} passHref>
+              <a>image</a>
+            </Link>
+          </div>
           <div className="flex w-1/4 justify-end">
             <div className="flex flex-row items-center justify-center text-center gap-2">
-              <a
-                href="#"
-                className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-              >
-                Blog
-              </a>
+              <Link href="/blog" passHref>
+                <a className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white">
+                  Blog
+                </a>
+              </Link>
               <a
                 href="#"
                 className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
