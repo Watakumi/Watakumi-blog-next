5c1c27e8594f5aa9c314da32edbea22eb1baca8b
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 03:41:49 +0900

    Layoutの重複を外す

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Layout from '../components/Layout';
 import Top from '../components/Top';
 
 export default function Home() {
@@ -11,9 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Layout>
-        <Top />
-      </Layout>
+      <Top />
     </div>
   );
 }
