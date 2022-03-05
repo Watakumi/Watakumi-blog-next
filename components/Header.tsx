fad2f984f3c2817d73beeb1b2091171d6a3fba83
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 14:31:33 +0900

    Tagsボタンで遷移できるようにする

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,12 +17,11 @@ export function Header(): JSX.Element {
                   Blog
                 </a>
               </Link>
-              <a
-                href="#"
-                className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-              >
-                Tags
-              </a>
+              <Link href="/tags" passHref>
+                <a className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white">
+                  Tags
+                </a>
+              </Link>
               <a
                 href="#"
                 className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
