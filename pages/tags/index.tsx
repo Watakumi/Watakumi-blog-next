70d9fba8937d6854d28d8112b806ec93404b0c1c
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 14:42:02 +0900

    余白の調整

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -2,7 +2,7 @@ import { TagsContent } from '../../components/TagsContent';
 
 export default function Tags(): JSX.Element {
   return (
-    <div className="mt-12">
+    <div className="mt-36">
       <TagsContent />
     </div>
   );
