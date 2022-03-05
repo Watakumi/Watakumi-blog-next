e1e72a2dff39f44d1e498e3cebe00a6dcb31ffb3
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-06 01:30:05 +0900

    markdownのデータを使える様にする

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,15 +1,29 @@
+import { InferGetStaticPropsType } from 'next';
 import { Card } from '../../components/Card';
 import { TitleContent } from '../../components/TitleContent';
+import { getAllPosts } from '../../lib/utils';
 
-export default function Blog(): JSX.Element {
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts(['slug', 'title', 'date', 'tags', 'summary']);
+  return {
+    props: { allPosts },
+  };
+};
+
+export default function Blog({ allPosts }: Props): JSX.Element {
   return (
     <div>
       <TitleContent title="All Posts" />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {allPosts?.map((post) => (
+        <Card
+          key={post.slug}
+          title={post.title}
+          summary={post.summary}
+          tags={post.tags}
+          date={post.date}
+        />
+      ))}
     </div>
   );
 }
