8920ba52179cc2fabf501766c13fd6ab94d5ad90
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 12:43:14 +0900

    TitleContextComponentの追加

---
+++ components/TitleContent.tsx
@@ -0,0 +1,10 @@
+export function TitleContent(): JSX.Element {
+  return (
+    <div>
+      <h1 className="text-4xl">Latest</h1>
+      <p className="pb-4 text-gray-800">
+        RubyやJavaScriptに関する記事を公開しています。
+      </p>
+    </div>
+  );
+}
--- components/Top.tsx
+++ components/Top.tsx
@@ -1,12 +1,10 @@
 import { Card } from './Card';
+import { TitleContent } from './TitleContent';
 
 export default function Top() {
   return (
     <div>
-      <h1 className="text-4xl">Latest</h1>
-      <p className="mb-4 text-gray-800">
-        RubyやJavaScriptに関する記事を公開しています。
-      </p>
+      <TitleContent />
       <Card />
       <Card />
       <Card />
