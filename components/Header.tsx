acf617a5357b83894c3b0e4953c25ec90e57c74f
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-05 15:36:20 +0900

    Projectsに遷移できるようにする

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,12 +22,11 @@ export function Header(): JSX.Element {
                   Tags
                 </a>
               </Link>
-              <a
-                href="#"
-                className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-              >
-                Projects
-              </a>
+              <Link href="/projects" passHref>
+                <a className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white">
+                  Projects
+                </a>
+              </Link>
             </div>
           </div>
         </div>
