c23795e67f05c7443940c5723b2512e49f642886
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-06 19:21:52 +0900

    code blockにシンタックスハイライトを適応する

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-module.exports = {
+const withTM = require('next-transpile-modules');
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['placehold.jp'],
   },
 };
+module.exports = withPlugins(
+  [
+    [
+      withTM,
+      {
+        transpileModules: ['react-syntax-highlighter'],
+      },
+    ],
+  ],
+  nextConfig
+);
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,6 +3,8 @@ import { getAllPosts, getPostBySlug } from '../../lib/utils';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import emoji from 'remark-emoji';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 export const getStaticPaths = async () => {
@@ -46,7 +48,28 @@ const Post: NextPage<Props> = ({ post }) => (
       ))}
     </ul>
     <section>
-      <ReactMarkdown remarkPlugins={[emoji, remarkGfm]}>
+      <ReactMarkdown
+        remarkPlugins={[emoji, remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                PreTag="div"
+                {...props}
+                style={darcula}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
         {post.content}
       </ReactMarkdown>
     </section>
