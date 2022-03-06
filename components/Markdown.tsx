511c9daa1c7ef63601cda01e517ed034cb2a1747
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-06 20:38:54 +0900

    markdown用のコンポーネントの追加

---
+++ components/Markdown.tsx
@@ -0,0 +1,37 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import emoji from 'remark-emoji';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+type Props = {
+  markdown: string;
+};
+export function Markdown({ markdown }: Props): JSX.Element {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[emoji, remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={darcula}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {markdown}
+    </ReactMarkdown>
+  );
+}
