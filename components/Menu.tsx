c8989f89afe0c9655730ea8a2915766dea000fe5
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-12 23:57:55 +0900

    レスポンシブ対応

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function Menu(): JSX.Element {
   return (
-    <div className="flex w-1/4 justify-end">
+    <div className="hidden w-1/4 justify-end sm:flex">
       <div className="flex flex-row items-center justify-center gap-2 text-center">
         <Link href="/blog" passHref>
           <a className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white">
