036d02b2dce5ca19683a0ef835b47b44edee2b83
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-12 23:42:24 +0900

    configを戻す

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-const withTM = require('next-transpile-modules');
-const withPlugins = require('next-compose-plugins');
-
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['placehold.jp'],
   },
 };
-module.exports = withPlugins(
-  [
-    [
-      withTM,
-      {
-        transpileModules: ['react-syntax-highlighter'],
-      },
-    ],
-  ],
-  nextConfig
-);
