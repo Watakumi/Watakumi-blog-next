06da89f8c42728cab120084141241603b8bdcd76
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-13 09:23:02 +0900

    Add siteMetadata

---
+++ data/siteMetadata.ts
@@ -0,0 +1,8 @@
+const metaData = {
+  title: 'Watakumi Blog',
+  author: 'Watakumi',
+  headerTitle: 'WatakumiBlog',
+  description: 'RubyやJacaScriptを中心とした記事をアップしています。',
+};
+
+export default metaData;
