b6d6eeb9d77ca3ffc0c7bbc03118c82bd02fef2f
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-13 09:48:54 +0900

    Use SEO Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { SEO } from '../components/SEO';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <SEO title="WatakumiBlog" />
       <Component {...pageProps} />
     </Layout>
   );
