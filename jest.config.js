f412f4834edcd243f2784698dedaf4eedd49cd51
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-13 19:54:07 +0900

    jestのconfigをjsにする

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-import nextJest from 'next/jest';
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // next.config.jsとテスト環境用の.envファイルが配置されたディレクトリをセット。基本は"./"で良い。
@@ -9,11 +9,11 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   // jest.setup.jsを作成する場合のみ定義。
   // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    // aliasを定義 （tsconfig.jsonのcompilerOptions>pathsの定義に合わせる）
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-  },
+  // moduleNameMapper: {
+  //   // aliasを定義 （tsconfig.jsonのcompilerOptions>pathsの定義に合わせる）
+  //   '^@/components/(.*)$': '<rootDir>/components/$1',
+  //   '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  // },
   testEnvironment: 'jest-environment-jsdom',
 };
 
