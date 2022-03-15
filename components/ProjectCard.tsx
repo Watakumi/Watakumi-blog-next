8c6a5d9693327872eee84a40e0283a07cfc2b1ba
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-15 23:06:49 +0900

    Projectのスタイルの変更

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -14,22 +14,31 @@ export function ProjectCard({
   href,
 }: Props): JSX.Element {
   return (
-    <div className="w-1/2 p-4">
-      <div className="">
+    <div className="p-4 md:w-1/2" style={{ maxWidth: '544px' }}>
+      <div className="overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700">
         <Link href={href} passHref>
           <a>
-            <Image alt={title} src={src} width={800} height={450} />
+            <Image
+              alt={title}
+              src={src}
+              width={800}
+              height={450}
+              className="object-cover object-center md:h-36 lg:h-48"
+            />
           </a>
         </Link>
-      </div>
-      <div className="w-auto space-y-4 p-6">
-        <h2 className="text-2xl">{title}</h2>
-        <p className="w-full text-gray-600">{description}</p>
-        <Link href={href} passHref>
-          <a className="inline-block py-2 text-cyan-600	hover:text-cyan-800">
-            Learn More &rarr;
-          </a>
-        </Link>
+        <div className="h-[250px] w-auto p-6">
+          <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+            {title}
+          </h2>
+          <p className="mb-3 max-w-none text-gray-600">{description}</p>
+
+          <Link href={href} passHref>
+            <a className="inline-block font-medium text-cyan-600 hover:text-cyan-800">
+              Learn More &rarr;
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   );
