9247da08ec22eedfbca5b9cdb0bb76aee4d1ac8a
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-25 18:24:56 +0900

    projectの詳細を更新

--- data/projectsdata.ts
+++ data/projectsdata.ts
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'WatakumiBlog',
-    description: `NextJSで作られたTechBlog。`,
+    description: `NextJSで作られたTechBlog。このブログのソースコードを公開しています。`,
     imgSrc:
       'https://watakumi-blog.s3.ap-northeast-1.amazonaws.com/watakumi-blog-logo-big.png',
     href: 'https://github.com/Watakumi/Watakumi-blog-next',
