597f9e98c5cbff65732aab9a10afeea6c0f79489
石渡拓海 <t-ishiwata@sight-visit.com>
2022-03-25 12:28:01 +0900

    トップ画像を標示する

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { InferGetStaticPropsType } from 'next';
 import { Card } from '../components/Card';
 import { TitleContent } from '../components/TitleContent';
 import { getAllPosts } from '../lib/utils';
+import Image from 'next/image';
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 export const getStaticProps = async () => {
   const allPosts = getAllPosts(['slug', 'title', 'date', 'tags', 'summary']);
@@ -19,6 +20,14 @@ export default function Home({ allPosts }: Props) {
         <meta name="description" content="This is Watakumi Blog." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="text-center">
+        <Image
+          src={'/images/logo.png'}
+          alt={'logo'}
+          width={120 * 8}
+          height={63 * 8}
+        />
+      </div>
       <TitleContent
         title={'Latest'}
         description={'RubyやJavaScriptに関する記事を公開しています。'}
