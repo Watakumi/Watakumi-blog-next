02a430f6d46ea5fa55b6390cb0504bd59440649f
石渡拓海 <t-ishiwata@sight-visit.com>
2022-04-03 17:34:24 +0900

    whitespaceの設定の追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import { Header } from './Header';
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-60">
-      <div className="flex flex-col justify-between h-screen">
+      <div className="flex h-screen flex-col justify-between whitespace-normal">
         <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
